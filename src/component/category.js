7c808d551ad9c0c9deaf320acd41fb49374c823f
Hafsarajput <hafsarajput580@gmail.com>
2023-12-09 22:20:43 +0500

    css for category

--- src/component/category.js
+++ src/component/category.js
@@ -1,6 +1,8 @@
 import { Container, Row, Col,ListGroup } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from './assets/images/cater.png';
+import '../component/catergory.css';
+
 
 const YourComponent = () => {
   return (
