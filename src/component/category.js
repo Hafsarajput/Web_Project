b3c86c98b9e37343fec4f014580624be471b2769
Hafsarajput <hafsarajput580@gmail.com>
2023-12-09 21:33:32 +0500

    update

--- src/component/category.js
+++ src/component/category.js
@@ -1,26 +1,68 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col,ListGroup } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from './assets/images/cater.png';
 
-const SplitScreen = () => {
+const YourComponent = () => {
   return (
-    <Container fluid>
+    <Container>
       <Row>
-        <Col xs={6}>
-        <img src={Image} alt='' style={{ maxWidth: '60px', height: 'auto' }} />
-          <div style={{ height: '50vh' }}>
-            Left Half
-          </div>
+        <Col>
+        <img src={Image} alt='' style={{ maxWidth: '400px', height: 'auto', paddingTop: '70px' }} />
         </Col>
-        <Col xs={6}>
-          {/* Content for the right half */}
-          <div style={{ height: '50vh' }}>
-            Right Half
-          </div>
+        <Col>
+        <h3 style={{ fontFamily: 'EB Garamond, serif', fontSize: '40px', maxWidth: '400px', height: 'auto', paddingTop: '100px', textAlign: 'center' }}>
+  <b>Keep your<br />Business organized<br />and on track.</b>
+</h3>
         </Col>
+        <Col>
+                        <ListGroup className='col-3'>
+                            <ListGroup.Item className='header'>
+                                <h3>Trending</h3>
+                            </ListGroup.Item>
+                            <ListGroup.Item className='content'>
+                                <h6>The Best Homemade Masks
+                                    for Face (keep the Pimples
+                                    Away)</h6>
+
+                                <p>Jane Cooper</p>
+                            </ListGroup.Item>
+                            <ListGroup.Item className='content'>
+                                <h6>17 Pictures of Medium
+                                    Length Hair in Layers That
+                                    Will Inspire Your New
+                                    Haircut</h6>
+
+                                <p>Wade Warren</p>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item className='content'>
+                                <h6>13 Amazing Poems from Shel
+                                    Silverstein with Valuable Life
+                                    Lessons</h6>
+
+                                <p>Esther Howard</p>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item className='content'>
+                                <h6>9 Half-up/half-down
+                                    Hairstyles for Long and
+                                    Medium Hair</h6>
+
+                                <p>Cameron Williamson</p>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item className='content'>
+                                <h6>Life Insurance And
+                                    Pregnancy: A Working
+                                    Mom’s Guide</h6>
+
+                                <p>Jenny Wilson</p>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Col>
       </Row>
     </Container>
   );
 };
 
-export default SplitScreen;
+export default YourComponent;
