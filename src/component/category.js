0b2a947d55317774788376db09dec08a533ee42a
Hafsarajput <hafsarajput580@gmail.com>
2023-12-12 21:44:24 +0500

    image added

--- src/component/category.js
+++ src/component/category.js
@@ -1,70 +1,81 @@
-import { Container, Row, Col,ListGroup } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import Image from './assets/images/cater.png';
 
-
-
-const YourComponent = () => {
+const ScreenDivision = () => {
   return (
-    <Container>
+    <Container fluid>
       <Row>
-        <Col>
-        <img src={Image} alt='' style={{ maxWidth: '400px', height: 'auto', paddingTop: '70px' }} />
+        <Col sm={6}>
+          {/* First division */}
+          <Container fluid>
+            <Row className="align-items-center">
+              <Col sm={6}>
+                {/* Image */}
+                <img
+                  src={Image}
+                  alt=''
+                  style={{ maxWidth: '150%', height: 'auto', paddingTop: '70px' }}
+                />
+              </Col>
+              <Col sm={6}>
+                {/* Text */}
+                <h3 style={{ fontFamily: 'EB Garamond, serif', fontSize: '40px', paddingTop: '80px', textAlign: 'center', paddingLeft: '30%' }}>
+                  <b>Keep your<br />Business organized<br />and on track.</b>
+                </h3>
+              </Col>
+              <p style={{ fontFamily: 'EB Garamond, serif', fontSize: '20px', paddingTop: '80px',  paddingLeft: '15%' }}><b>CATEGORY : BUSINESS</b></p>
+            </Row>
+           {/* cat 1 */}
+            <Container>
+        <Row className='align-items-center'>
+          <Col xs={12} md={4}>
+            <img src={Image} alt='' style={{ maxWidth: '80px', height: 'auto' }} />
+          </Col>
+          <Col xs={12} md={8}>
+            <p style={{ fontSize: '10px', marginBottom: '5px' }}>CULTURE . Jul 5th '22</p>
+            <p style={{ fontSize: '15px', marginBottom: '20px' }}>5 Great Startup Tips for Female Founders</p>
+          </Col>
+        </Row>
+      </Container>
+ {/* cat 2 */}
+      <Container>
+        <Row className='align-items-center'>
+          <Col xs={12} md={4}>
+            <img src={Image} alt='' style={{ maxWidth: '80px', height: 'auto' }} />
+          </Col>
+          <Col xs={12} md={8}>
+            <p style={{ fontSize: '10px', marginBottom: '5px' }}>CULTURE . Jul 5th '22</p>
+            <p style={{ fontSize: '15px', marginBottom: '20px' }}>5 Great Startup Tips for Female Founders</p>
+          </Col>
+        </Row>
+      </Container>
+ {/* cat 3 */}
+      <Container>
+        <Row className='align-items-center'>
+          <Col xs={12} md={4}>
+            <img src={Image} alt='' style={{ maxWidth: '80px', height: 'auto' }} />
+          </Col>
+          <Col xs={12} md={8}>
+            <p style={{ fontSize: '10px', marginBottom: '5px' }}>CULTURE . Jul 5th '22</p>
+            <p style={{ fontSize: '15px', marginBottom: '20px' }}>5 Great Startup Tips for Female Founders</p>
+          </Col>
+        </Row>
+      </Container>
+          </Container>
+          <div style={{ height: '200px', paddingLeft: '50px' }}></div>
         </Col>
-        <Col>
-        <h3 style={{ fontFamily: 'EB Garamond, serif', fontSize: '40px', maxWidth: '400px', height: 'auto', paddingTop: '100px', textAlign: 'center' }}>
-  <b>Keep your<br />Business organized<br />and on track.</b>
-</h3>
+
+        {/* Second division moved to the right */}
+        <Col sm={6}>
+        <div style={{ height: '200px', backgroundColor: 'lightblue', width: '150px', textAlign: 'right' }}>
+            {/* Content for the second part */}
+            Second Part
+          </div>
         </Col>
-        <Col>
-                        <ListGroup className='col-3'>
-                            <ListGroup.Item className='header'>
-                                <h3>Trending</h3>
-                            </ListGroup.Item>
-                            <ListGroup.Item className='content'>
-                                <h6>The Best Homemade Masks
-                                    for Face (keep the Pimples
-                                    Away)</h6>
-
-                                <p>Jane Cooper</p>
-                            </ListGroup.Item>
-                            <ListGroup.Item className='content'>
-                                <h6>17 Pictures of Medium
-                                    Length Hair in Layers That
-                                    Will Inspire Your New
-                                    Haircut</h6>
-
-                                <p>Wade Warren</p>
-                            </ListGroup.Item>
-
-                            <ListGroup.Item className='content'>
-                                <h6>13 Amazing Poems from Shel
-                                    Silverstein with Valuable Life
-                                    Lessons</h6>
-
-                                <p>Esther Howard</p>
-                            </ListGroup.Item>
-
-                            <ListGroup.Item className='content'>
-                                <h6>9 Half-up/half-down
-                                    Hairstyles for Long and
-                                    Medium Hair</h6>
-
-                                <p>Cameron Williamson</p>
-                            </ListGroup.Item>
-
-                            <ListGroup.Item className='content'>
-                                <h6>Life Insurance And
-                                    Pregnancy: A Working
-                                    Mom’s Guide</h6>
-
-                                <p>Jenny Wilson</p>
-                            </ListGroup.Item>
-                        </ListGroup>
-                    </Col>
       </Row>
     </Container>
   );
 };
 
-export default YourComponent;
+export default ScreenDivision;
